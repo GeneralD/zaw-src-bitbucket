960781500aac8c61dc8d25b61b38e6765bf6fdc5
GeneralD <yumejustice@gmail.com>
2017-12-02 22:24:50 +0900

    fix a bug

--- src/bitbucket.zsh
+++ src/bitbucket.zsh
@@ -4,7 +4,7 @@ function zaw-src-bitbucket() {
     if which jq 1>/dev/null || brew install jq
     local space=$'\uf116'
     local bitbucket=$'\uf171'
-    local l=`\curl -s "https://api.bitbucket.org/2.0/repositories/${ZAW_BITBUCKET_USER}?role=owner&pagelen=100" -u "${ZAW_GITHUB_USER}:${ZAW_BITBUCKET_PASSWORD}"`
+    local l=`\curl -s "https://api.bitbucket.org/2.0/repositories/${ZAW_BITBUCKET_USER}?role=owner&pagelen=100" -u "${ZAW_BITBUCKET_USER}:${ZAW_BITBUCKET_PASSWORD}"`
     candidates=(`echo $l | \jq --raw-output '.values[].links.clone[1].href'`)
     cand_descriptions=(`echo $l | \jq --raw-output '.values[].full_name' | sed "s/^/${bitbucket}${space}/g"`)
 
