82960336507a1031179032e5437759326dcfb053
GeneralD <yumejustice@gmail.com>
2017-12-08 13:17:08 +0900

    add browse sub-command

--- src/bitbucket.zsh
+++ src/bitbucket.zsh
@@ -4,8 +4,10 @@ function zaw-src-bitbucket() {
     if which jq 1>/dev/null || brew install jq
     local space=$'\uf116'
     local bitbucket=$'\uf171'
+    local index
+    [[ $BITBUCKET_SCM == 'git' ]] && index=1 || index=0
     local l=`\curl -s "https://api.bitbucket.org/2.0/repositories/${ZAW_BITBUCKET_USER}?role=owner&pagelen=100" -u "${ZAW_BITBUCKET_USER}:${ZAW_BITBUCKET_PASSWORD}"`
-    candidates=(`echo $l | \jq --raw-output '.values[].links.clone[1].href'`)
+    candidates=(`echo $l | \jq --raw-output ".values[].links.clone[$index].href"`)
     cand_descriptions=(`echo $l | \jq --raw-output '.values[].full_name' | sed "s/^/${bitbucket}${space}/g"`)
 
     if which ghq 1>/dev/null; then
@@ -13,8 +15,8 @@ function zaw-src-bitbucket() {
         act_descriptions=('ghq get')
     fi
 
-    actions+=('zaw-src-bitbucket-clone' 'zaw-callback-append-to-buffer' 'zaw-callback-replace-buffer')
-    act_descriptions+=('git clone' 'append to edit buffer' 'replace edit buffer')
+    actions+=('zaw-src-bitbucket-clone' 'zaw-src-bitbucket-browse' 'zaw-callback-append-to-buffer' 'zaw-callback-replace-buffer')
+    act_descriptions+=('git clone' 'browse' 'append to edit buffer' 'replace edit buffer')
 }
 
 function zaw-src-bitbucket-ghq() {
@@ -24,6 +26,12 @@ function zaw-src-bitbucket-ghq() {
 
 function zaw-src-bitbucket-clone() {
     BUFFER="git clone ${(j:; :)@}"
+    zle accept-line
+}
+
+function zaw-src-bitbucket-browse() {
+    local repo=`echo ${(j:; :)@} | egrep -o "[/:][^/]+/[^/.]+.git$" | cut -f 1 -d '.' | sed 's/^[:\/]//g'`
+    BUFFER="open https://bitbucket.org/$repo"
     zle accept-line
 }
 
--- src/variables.zsh
+++ src/variables.zsh
@@ -2,3 +2,4 @@
 
 ZAW_BITBUCKET_USER=${ZAW_BITBUCKET_USER:-`\cat .bitbucket 2>/dev/null | \grep username | \awk '{ print $NF }'`}
 ZAW_BITBUCKET_PASSWORD=${ZAW_BITBUCKET_PASSWORD:-`\cat .bitbucket 2>/dev/null | \grep password | \awk '{ print $NF }'`}
+ZAW_BITBUCKET_SCM=${ZAW_BITBUCKET_SCM:-`\cat ~/.bitbucket 2>/dev/null | \grep scm | \awk '{ print $NF }'`}
